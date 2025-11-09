71c8c211503f1793212fd3af2ae054629ea5569c
kj-kenan <kenan.altunbas@std.bogazici.edu.tr>
2025-11-10 00:27:41 +0300

    allowed sites update

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -76,6 +76,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://the-hive-frontend.netlify.app",
+    "https://jolly-churros-6487ef.netlify.app",
 ]
 
 
